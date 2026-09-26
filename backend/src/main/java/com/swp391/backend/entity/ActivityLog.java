34cf5b4136543e6624f5668c4fc5feae53c64d8c
jaykilz <laiduykhanh72@gmail.com>
2026-09-26 12:26:52 +0700

    feat(login): add secret key, login by phone

--- backend/src/main/java/com/swp391/backend/entity/ActivityLog.java
+++ backend/src/main/java/com/swp391/backend/entity/ActivityLog.java
@@ -32,6 +32,67 @@ public class ActivityLog {
     @Column(columnDefinition = "NVARCHAR(50)")
     private String ipAddress;
 
+    public ActivityLog() {
+    }
+
+    public ActivityLog(Long activityLogId, Long accountId, String action, String description, LocalDateTime createdAt,
+            String ipAddress) {
+        this.activityLogId = activityLogId;
+        this.accountId = accountId;
+        this.action = action;
+        this.description = description;
+        this.createdAt = createdAt;
+        this.ipAddress = ipAddress;
+    }
+
+    public Long getActivityLogId() {
+        return activityLogId;
+    }
+
+    public void setActivityLogId(Long activityLogId) {
+        this.activityLogId = activityLogId;
+    }
+
+    public Long getAccountId() {
+        return accountId;
+    }
+
+    public void setAccountId(Long accountId) {
+        this.accountId = accountId;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getIpAddress() {
+        return ipAddress;
+    }
+
+    public void setIpAddress(String ipAddress) {
+        this.ipAddress = ipAddress;
+    }
+
     
 
     
--- backend/src/main/java/com/swp391/backend/repository/AccountRepository.java
+++ backend/src/main/java/com/swp391/backend/repository/AccountRepository.java
@@ -10,4 +10,7 @@ import java.util.Optional;
 public interface AccountRepository extends JpaRepository<Account, Long> {
 
     Optional<Account> findByEmail(String email);
+
+    Optional<Account> findByPhone(String phone);
+
 }
